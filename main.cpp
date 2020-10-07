66cea71118e79a0794726e746430570a06817881
Angel Armando <angel.gomez1394@alumnos.udg.mx>
2020-10-06 21:42:52 -0500

    probando setters y getters

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main(){
     Computadora PC1 = Computadora(2048, "Windows", "Intel", "Armando");
+    Computadora PC2;
 
+    PC2.setRAM(1024);
+    cout <<PC2.getRAM() <<endl;
+    cout <<PC1.getRAM() <<endl;
 
    return 0; 
 }
