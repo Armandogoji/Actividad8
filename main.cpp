226efddf273defe9e03a42f3f128a38034fe99e4
Angel Armando <angel.gomez1394@alumnos.udg.mx>
2020-10-06 22:42:56 -0500

    Usando la clase laboratorio para almacenar las computadoras

--- main.cpp
+++ main.cpp
@@ -1,25 +1,43 @@
 #include <iostream>
 #include "computadora.h"
+#include "Laboratorio.h"
 using namespace std;
 
 
 int main(){
-    Computadora PC1 = Computadora(2048, "Windows", "Intel", "Armando");
+    Computadora PC1 = Computadora(2048, "Windows", "Intel", "Roberto");
     Computadora PC2;
+    Computadora PC3 = Computadora(4096, "Mac", "Intel", "Carlos");
+    Computadora PC4;
 
     PC2.setRAM(1024);
     PC2.setSO("Linux");
     PC2.setProcesador("AMD");
-    PC2.setnombre_equipo("Angel");
-    cout <<PC2.getRAM() <<endl;
-    cout <<PC2.getSO() <<endl;
-    cout <<PC2.getProcesador() <<endl;
-    cout <<PC2.getnombre_equipo() <<endl;
+    PC2.setnombre_equipo("Amanda");
+    
+    PC4.setRAM(8192);
+    PC4.setSO("Windows");
+    PC4.setProcesador("AMD");
+    PC4.setnombre_equipo("Armando");
 
-    cout <<PC1.getRAM() <<endl;
-    cout <<PC1.getSO() <<endl;
-    cout <<PC1.getProcesador() <<endl;
-    cout <<PC1.getnombre_equipo() <<endl;
+    Laboratorio lab;
+    lab.agregarComputadora(PC1);
+    lab.agregarComputadora(PC2);
+    lab.agregarComputadora(PC3);
+    lab.agregarComputadora(PC4);
+
+    lab.mostrar();
+    
+    
+    //cout <<PC2.getRAM() <<endl;
+    //cout <<PC2.getSO() <<endl;
+    //cout <<PC2.getProcesador() <<endl;
+    //cout <<PC2.getnombre_equipo() <<endl;
+
+    //cout <<PC1.getRAM() <<endl;
+    //cout <<PC1.getSO() <<endl;
+    //cout <<PC1.getProcesador() <<endl;
+    //cout <<PC1.getnombre_equipo() <<endl;
 
    return 0; 
 }
